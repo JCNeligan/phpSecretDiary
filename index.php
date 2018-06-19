9bb242da110b9e3e95d671622080a6983b9af5c5
JackNel <jcneligan@gmail.com>
2018-06-19 17:08:34 -0400

    Add bootsrap cdn and test first form

--- index.php
+++ index.php
@@ -1,24 +1,31 @@
 <?php
 
+if (array_key_exists("submit-signup", $_POST)) {
+    print_r($_POST);
+} else {
+    echo "Nope!";
+}
+
 ?>
 
 <html>
   <head>
   <title>Secret Diary</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
 
   <body>
     <form method="post">
-      <input type="text" name="email-signup" placeholder="Your Email"/>
+      <input type="email" name="email-signup" placeholder="Your Email"/>
       <input type="password" name="password-signup" placeholder="Your Password"/>
       <input type="checkbox" name="persist-signup"/>
-      <input type="submit" value="Sign Up!"/>
+      <input type="submit" name="submit-signup" value="Sign Up!"/>
     </form>
     <form method="post">
       <input type="text" name="email-login" placeholder="Your Email"/>
       <input type="password" name="password-login" placeholder="Your Password"/>
       <input type="checkbox" name="persist-login"/>
-      <input type="submit" value="Log In!"/>
+      <input type="submit" name="submit-login" value="Log In!"/>
     </form>
   </body>
-</html
+</html>
