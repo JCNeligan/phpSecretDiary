907da37b56fa8c370910b6c31c5251f58911c969
JackNel <jcneligan@gmail.com>
2018-06-19 16:55:54 -0400

    initial import

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<html>
+  <head>
+  <title>Secret Diary</title>
+  </head>
+
+  <body>
+    <form method="post">
+      <input type="text" name="email-signup" placeholder="Your Email"/>
+      <input type="password" name="password-signup" placeholder="Your Password"/>
+      <input type="checkbox" name="persist-signup"/>
+      <input type="submit" value="Sign Up!"/>
+    </form>
+    <form method="post">
+      <input type="text" name="email-login" placeholder="Your Email"/>
+      <input type="password" name="password-login" placeholder="Your Password"/>
+      <input type="checkbox" name="persist-login"/>
+      <input type="submit" value="Log In!"/>
+    </form>
+  </body>
+</html
