fd9d67a937d7bb0aab3ee9dd6aa15f5e296ccb50
JackNel <jcneligan@gmail.com>
2018-06-22 17:38:07 -0400

    Basic setup for login form

--- index.php
+++ index.php
@@ -12,44 +12,48 @@ if (array_key_exists("logout", $_GET)) {
     header("Location: loggedinpage.php");
 }
 
-if (array_key_exists("submit-signup", $_POST)) {
+if (array_key_exists("submit", $_POST)) {
 
     $link = mysqli_connect("localhost", "root", "", "secret_diary");
     if (mysqli_connect_error()) {
         die("Error connecting to database");
     }
 
-    if (!$_POST["email-signup"]) {
+    if (!$_POST["email"]) {
         $error = "Email address is required<br>";
     }
 
-    if (!$_POST["password-signup"]) {
+    if (!$_POST["password"]) {
         $error = "Password is required<br>";
     }
 
     if ($error != "") {
         $error = "<p>There were error(s) in your form:</p>" . $error;
     } else {
-        $query = "SELECT `id` FROM `users` WHERE `email` = '" . mysqli_real_escape_string($link, $_POST['email-signup']) . "' LIMIT 1";
-        $result = mysqli_query($link, $query);
-        if (mysqli_num_rows($result) > 0) {
-            $error = "<p>That email address has already been taken.</p>";
-        } else {
-            $query = "INSERT INTO `users` (`email`, `password`) VALUES ('" . mysqli_real_escape_string($link, $_POST['email-signup']) . "', '" . mysqli_real_escape_string($link, $_POST['password-signup']) . "')";
-
-            if (!mysqli_query($link, $query)) {
-                $error = "<p>Could not complete signup at this time, please try again later</p>";
+        if ($_POST["signUp"] == "1") {
+            $query = "SELECT `id` FROM `users` WHERE `email` = '" . mysqli_real_escape_string($link, $_POST['email']) . "' LIMIT 1";
+            $result = mysqli_query($link, $query);
+            if (mysqli_num_rows($result) > 0) {
+                $error = "<p>That email address has already been taken.</p>";
             } else {
-                $query = "UPDATE `users` SET password = '" . md5(md5(mysqli_insert_id($link)) . $_POST['password-signup']) . "' WHERE id = " . mysqli_insert_id($link) . " LIMIT 1";
-                mysqli_query($link, $query);
-                $_SESSION["id"] = mysqli_insert_id($link);
-                if ($_POST["persist-signup" == 1]) {
-                    setcookie("id", mysqli_insert_id($link), time() + 60 * 60 * 48);
+                $query = "INSERT INTO `users` (`email`, `password`) VALUES ('" . mysqli_real_escape_string($link, $_POST['email']) . "', '" . mysqli_real_escape_string($link, $_POST['password']) . "')";
+
+                if (!mysqli_query($link, $query)) {
+                    $error = "<p>Could not complete signup at this time, please try again later</p>";
+                } else {
+                    $query = "UPDATE `users` SET password = '" . md5(md5(mysqli_insert_id($link)) . $_POST['password']) . "' WHERE id = " . mysqli_insert_id($link) . " LIMIT 1";
+                    mysqli_query($link, $query);
+                    $_SESSION["id"] = mysqli_insert_id($link);
+                    if ($_POST["persist" == 1]) {
+                        setcookie("id", mysqli_insert_id($link), time() + 60 * 60 * 48);
+                    }
+                    header("Location: loggedinpage.php");
+
                 }
-                header("Location: loggedinpage.php");
 
             }
-
+        } else {
+            print_r($_POST);
         }
     }
 }
@@ -67,16 +71,18 @@ if (array_key_exists("submit-signup", $_POST)) {
   <div id="error"><?php echo $error; ?></div>
 
     <form method="post">
-      <input type="email" name="email-signup" placeholder="Your Email"/>
-      <input type="password" name="password-signup" placeholder="Your Password"/>
-      <input type="checkbox" name="persist-signup"/>
-      <input type="submit" name="submit-signup" value="Sign Up!"/>
+      <input type="email" name="email" placeholder="Your Email"/>
+      <input type="password" name="password" placeholder="Your Password"/>
+      <input type="checkbox" name="persist"/>
+      <input type="hidden" name="signUp" value="1"/>
+      <input type="submit" name="submit" value="Sign Up!"/>
     </form>
     <form method="post">
-      <input type="text" name="email-login" placeholder="Your Email"/>
-      <input type="password" name="password-login" placeholder="Your Password"/>
-      <input type="checkbox" name="persist-login"/>
-      <input type="submit" name="submit-login" value="Log In!"/>
+      <input type="text" name="email" placeholder="Your Email"/>
+      <input type="password" name="password" placeholder="Your Password"/>
+      <input type="checkbox" name="persist"/>
+      <input type="hidden" name="signUp" value="0"/>
+      <input type="submit" name="submit" value="Log In!"/>
     </form>
   </body>
 </html>
