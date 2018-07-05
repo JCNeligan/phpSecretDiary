edcf66b130e08397db41d369f7f1cd4c6a89ffe7
JackNel <jcneligan@gmail.com>
2018-07-04 22:52:57 -0400

    Add some form styling and background image

--- index.php
+++ index.php
@@ -79,29 +79,57 @@ if (array_key_exists("submit", $_POST)) {
 
 ?>
 
-<html>
-  <head>
-  <title>Secret Diary</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" crossorigin="anonymous">
+        <title>Secret Diary</title>
+    </head>
 
-  <body>
+    <body>
+        <div class="container">
+            <h1>Secret Diary</h1>
+            <div id="error"><?php echo $error; ?></div>
 
-  <div id="error"><?php echo $error; ?></div>
+            <form method="post">
+                <div class="form-group">
+                    <input class="form-control" type="email" name="email" placeholder="Your Email"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" placeholder="Your Password"/>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="persist" id="check1"/>
+                    <label class="form-check-label" for="check1">Stay logged in?</label>
+                </div>
+                <div class="form-group">
+                    <input type="hidden" name="signUp" value="1"/>
+                    <input class="btn btn-success" type="submit" name="submit" value="Sign Up!"/>
+                </div>
+            </form>
+            <form method="post">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="email" placeholder="Your Email"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" placeholder="Your Password"/>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="persist" id="check2"/>
+                    <label class="form-check-label" for="check2">Stay logged in?</label>
+                </div>
+                <div class="form-group">
+                    <input type="hidden" name="signUp" value="0"/>
+                    <input class="btn btn-success" type="submit" name="submit" value="Log In!"/>
+                </div>
+            </form>
+        </div>
 
-    <form method="post">
-      <input type="email" name="email" placeholder="Your Email"/>
-      <input type="password" name="password" placeholder="Your Password"/>
-      <input type="checkbox" name="persist"/>
-      <input type="hidden" name="signUp" value="1"/>
-      <input type="submit" name="submit" value="Sign Up!"/>
-    </form>
-    <form method="post">
-      <input type="text" name="email" placeholder="Your Email"/>
-      <input type="password" name="password" placeholder="Your Password"/>
-      <input type="checkbox" name="persist"/>
-      <input type="hidden" name="signUp" value="0"/>
-      <input type="submit" name="submit" value="Log In!"/>
-    </form>
-  </body>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
 </html>
